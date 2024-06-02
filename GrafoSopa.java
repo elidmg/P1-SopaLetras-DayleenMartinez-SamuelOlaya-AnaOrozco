8cd1cd0b0488f518be53f04ca0ac3f504a56c690
elidmg <123570102+elidmg@users.noreply.github.com>
2024-06-02 11:46:28 -0400

    Add files via upload
    Nueva Versión, ya lista de la clase Grafo Matriz.

--- GrafoSopa.java
+++ GrafoSopa.java
@@ -2,95 +2,176 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.proyectoo;
+package Clases;
 
-/**
+import javax.swing.JOptionPane;
+
+/** Clase grafo Sopa, que representa la matriz completa
  *
- * @author ani
+ * @author ani y Dayleen
  */
-public class GrafoSopa {
-    int numLetras;
-    int maxLetras;
-    Letra [] letras;
-    int [][] matAd;
+public class GrafoSopa { //Estoy implementando grafo dentro de la matriz
+    private int numLetras;  //Numero de vertices del grafo
+    private int maxLetras; // Tamano maximo de la tabla
+    private Letra [] letras; 
+    private int [][] matrizAdy;  //Matriz de adyacencia de los vectores 
     
-    
-    public GrafoSopa(){
-        this.maxLetras = 16;
+    //Constructor de inicializacion que se le pas apor argumentos numero de vertices
+    public GrafoSopa(int v){
+        numLetras = 16; //numero de vertices
+        maxLetras = v; //max de la tabla
+        matrizAdy = new int[v][v]; //Boleano de adyacencia entre vertices        
     } 
+        
+    /**Insertar un Vertice en mi matriz, este evalua la capacidad de 
+     * los vertices, si no hay disponibilidad lanza un mensaje,
+     * Si hay disponibilidad lo inserta
+     * @param v
+     *
+     */
+    //USO GET A SET PORQUE ES PRIVADO
+    public void InsertarLetra(int v){
+        if(v > getMaxLetras() - getNumLetras()){
+            JOptionPane.showMessageDialog(null, "Error, se supera el numero de vertices","Error",JOptionPane.ERROR_MESSAGE);
+        }
+        else{
+            for(int i= 0; i <getNumLetras() +v; i++){
+                for(int j= getNumLetras(); j < getNumLetras() + v; j++){
+                    getMatrizAdy() [i][j] = getMatrizAdy() [i][j] = 0;
+                }
+            }
+        }
+        setNumLetras(getNumLetras() + v);
+    } 
+    //OJO ESTO CAMBIA EL INDICE DE LOS VERTICES
     
-    //añadir letra
-    public void nuevaLetra(String letra){
-        boolean esta = numLetra(letra)>=0;
-        if (!esta){
-            Letra v= new Letra(letra);
-            v.asigLetra(numLetras);
-            letras[numLetras++]=v;
+    /**Eliminar Vertice, evalua que este dentro del rango
+     * Si no esta manda errror
+     * Si lo esta, Recorre la matriz hasta llegar a su direccion lo elimina 
+     * colocando -1 
+     * y cambia las adyacencias al siguiente vertice para quitar el que se buscaba
+     * @param v
+     */    
+    public void EliminarLetra(int v){
+        if(v >= getNumLetras()){
+            JOptionPane.showMessageDialog(null, "Error, se supera el numero de vertices","Error",JOptionPane.ERROR_MESSAGE);
+        }else{
+            for (int i = v; i < getNumLetras(); i++) {
+                for (int j = 0; j < getNumLetras(); j++){
+                   if(i == getNumLetras() -1 || j == getNumLetras() -1){
+                        getMatrizAdy()[i][j] = getMatrizAdy()[j][i] = 0;
+                   }else{
+                        getMatrizAdy()[i][j] = getMatrizAdy()[i+1][j];
+                        getMatrizAdy()[j][i] = getMatrizAdy()[i][j];
+                       
+                   }
+                }
+            }
+             setNumLetras(getNumLetras()- 1);
         }
         
-    } 
-    //busca el vertice en el array, si no lo encuentra retorna false, y si la encontra true
-    public int numLetra(String vs){
-        Letra v= new Letra(vs);
-        boolean encontrado=false;
-        int i = 0;
-        for(;(i<numLetras)&& !encontrado;)
-        {
-            encontrado = letras[i].equals(v);
-            if (!encontrado) i++;
-          
-        }
-        return (i<numLetras)? i: -1;
-               
     }
     
-    /** Arconuevo
-     * @param a
-     * @param b 
-     * @throws java.lang.Exception 
+    
+    /** Insertar una arista dentro de la matriz
+     * @param i
+     * @param j 
      * @param: String a, String b 
      * 
      */
-    /*
-    public void nuevoArco(String a,String b)throws Exception{
-        int va,vb;
-        va = numLetra(a);
-        vb = numLetra(b);
-        if (va<0 ||vb<0) throw new Exception ("La letra no existe");
-        matAd [va][vb]=1;
-        
-        
+    //En caso de que
+    //se trate de un grafo no dirigido, debe recordarse que 
+    //la arista (i, j) es igual a la arista (j, i) para que 
+    //la matriz mantenga la propiedad de la simetría.
+    
+    public void InsertarArista(int i, int j){
+        //Evalua la existencia de ambos vertices
+        if (i < 0||j <0) {
+            JOptionPane.showMessageDialog(null, "Error, No existe","Error",JOptionPane.ERROR_MESSAGE);
+        }
+        //Si existe ambos son adyacentes por tanto comparten una arista como 1
+        //Colocamos 1 porque nosotros no tenemos un peso 
+        getMatrizAdy()[i][j] = getMatrizAdy()[j][i] = 1;
+    }    
+    
+    
+    /** Eliminar Aristas, Si se quiere eliminar aristas entre dos 
+     * vertices se coloca 0, para decir que no hay una conexion entre ellas.
+     * @param i
+     * @param j
+     */
+    public void EliminarArista(int i, int j) {
+        if (i < 0||j <0){JOptionPane.showMessageDialog(null, "Error, No existe","Error",JOptionPane.ERROR_MESSAGE);
+        }
+        getMatrizAdy()[i][j] = getMatrizAdy()[j][i] = 0;
     }
-    */
-    /**Arconuevo
-     * 
-     * @param va
-     * @param vb
+    
+    /**
+     * Imprimir Matriz,a tarvez de consola para saber como va quedando
+     * @return 
+     */
+    public String print(){
+        String matrix = "";
+        System.out.println("Nuestra matriz tiene " + getNumLetras() + " vertices \n");
+        for (int i = 0; i < getNumLetras(); i++) {
+            for (int j = 0; j < getNumLetras(); j++) {
+                matrix += "[" + getMatrizAdy()[i][j] + "]" + " ";
+            }
+            matrix = matrix + "\n";
+        }
+        return matrix;
+    }
+    
+    
+    /**true Determina si dos letras forman un arco, 
+     * es decir si el elemento de la matriz es 1,
+     * si hay adyacencia entre ellos     
+     * @param i
+     * @param j
+     * @return
      * @throws Exception 
      */
-    public void nuevoArco(int va,int vb)throws Exception{
-        if (va<0||vb<0) throw new Exception("La letra no existe");
-        matAd[va][vb]=1;
-       
+    
+    //Trows Exception Funciona como un JOptions 
+    //hrows declara qué excepción puede lanzar un 
+    //método en caso de error.
+    public boolean adyacene(int i,int j)throws Exception{
+        if (i<0||j<0) throw new Exception ("Vertice no existe");
+        return matrizAdy[i][j]==1;
     }
-    /** @return true Determina si dos letras forman un arco, es decir si el elemento de la matriz es 1 
-     * 
-     * @param a
-     * @param b
-     * @throws Exception 
-     */
-    public boolean adyacente(String a, String b)throws Exception{
-        int va,vb;
-        va = numLetra(a);
-        vb = numLetra(b);
-        if (va<0||vb<0)throw new Exception("La letra no existe");
-        return matAd[va][vb] ==1;
-        
+
+    
+    
+    public int getNumLetras() {
+        return numLetras;
     }
-    public boolean adyacene(int va,int vb)throws Exception{
-        if (va<0||vb<0) throw new Exception ("Vertice no existe");
-        return matAd[va][vb]==1;
-        
+
+    public void setNumLetras(int numLetras) {
+        this.numLetras = numLetras;
+    }
+
+    public int getMaxLetras() {
+        return maxLetras;
+    }
+
+    public void setMaxLetras(int maxLetras) {
+        this.maxLetras = maxLetras;
+    }
+
+    public Letra[] getLetras() {
+        return letras;
+    }
+
+    public void setLetras(Letra[] letras) {
+        this.letras = letras;
+    }
+
+    public int[][] getMatrizAdy() {
+        return matrizAdy;
+    }
+
+    public void setMatrizAdy(int[][] matrizAdy) {
+        this.matrizAdy = matrizAdy;
     }
             
     
